6d8e2a38baccb0ad273b32135541e1faf3ecd652
helodv <hloureiro.dv@outlook.com>
2024-05-22 17:17:19 -0300

    main

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-// array de productos
-let productos = [
-    // id, nombre, marca, modelo, categoria, stock, precio, imagen
-    { id: 1, nombre: "MSI B450 Gaming Max", marca: "MSI", modelo: 'B450 Gaming Max', categoria: "motherboard", stock: 0, precio: 45199, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 3, nombre: "GIGABYTE B450 AORUS PRO", marca: "GIGABYTE", modelo: "B450 AORUS PRO", categoria: "motherboard", stock: 12, precio: 8999, imagen: "../assets/images/productos/mb_gigabyte_b450_aorus_pro.png" },
-    { id: 2, nombre: "ASUS ROG Strix B550-F Gaming", marca: "ASUS", modelo: "ROG Strix B550-F", categoria: "motherboard", stock: 1, precio: 18999, imagen: "../assets/images/productos/mb_asus_rog_strix_gaming.webp" },
-    { id: 4, nombre: "MSI MPG Z590 GAMING EDGE WIFI", marca: "MSI", modelo: "MPG Z590 GAMING EDGE WIFI", categoria: "motherboard", stock: 6, precio: 21999, imagen: "../assets/images/productos/msi_mpg_z590_gaming_edge_wifi.png" },
-    { id: 5, nombre: "ASRock B560M-ITX/ac", marca: "ASRock", modelo: "B560M-ITX/ac", categoria: "motherboard", stock: 3, precio: 12999, imagen: "../assets/images/productos/mb_asrock_b560m_itx.png" },
-    { id: 6, nombre: "AMD Ryzen 7 5800X", marca: "AMD", modelo: "Ryzen 7 5800X", categoria: "procesador", stock: 10, precio: 29999, imagen: "../assets/images/productos/ryzen7.webp" },
-    { id: 7, nombre: "Intel Core i5-11600K", marca: "Intel", modelo: "Core i5-11600K", categoria: "procesador", stock: 7, precio: 24999, imagen: "../assets/images/productos/i5.jpg" },
-    { id: 8, nombre: "AMD Ryzen 9 5950X", marca: "AMD", modelo: "Ryzen 9 5950X", categoria: "procesador", stock: 4, precio: 69999, imagen: "../assets/images/productos/ryzen9.webp" },
-    { id: 9, nombre: "Intel Core i9-11900KF", marca: "Intel", modelo: "Core i9-11900KF", categoria: "procesador", stock: 2, precio: 79999, imagen: "../assets/images/productos/i9.jpg" },
-    { id: 10, nombre: "AMD Ryzen 5 5600G", marca: "AMD", modelo: "Ryzen 5 5600G", categoria: "procesador", stock: 15, precio: 17999, imagen: "../assets/images/productos/ryzen5.jpg" },
-    { id: 11, nombre: "Corsair Vengeance LPX 16GB DDR4 3200MHz", marca: "Corsair", modelo: "Vengeance LPX", categoria: "memoria RAM", stock: 20, precio: 6999, imagen: "../assets/images/productos/ram_corsair_vengeance_lpx_16gb_dddr4_3200mhz.jpg" },
-    { id: 12, nombre: "Crucial Ballistix 32GB DDR4 3600MHz", marca: "Crucial", modelo: "Ballistix", categoria: "memoria RAM", stock: 15, precio: 12999, imagen: "../assets/images/productos/ram_crucial_ballistix_32gb_ddr4_3600mhz.jpg" },
-    { id: 13, nombre: "G.Skill Ripjaws V 64GB DDR4 3200MHz", marca: "G.Skill", modelo: "Ripjaws V", categoria: "memoria RAM", stock: 8, precio: 24999, imagen: "../assets/images/productos/ram_gskill_ripjaws_v_64gb_ddr4_3200mhz.jpg" },
-    { id: 14, nombre: "HyperX Fury RGB 16GB DDR4 2666MHz", marca: "HyperX", modelo: "Fury RGB", categoria: "memoria RAM", stock: 25, precio: 7999, imagen: "../assets/images/productos/ram_hyperx_fury_rgb_16gb_ddr4_2666mhz.jpg" },
-    { id: 15, nombre: "Team T-Force Delta RGB 32GB DDR4 3000MHz", marca: "Team T-Force", modelo: "Delta RGB", categoria: "memoria RAM", stock: 10, precio: 10999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 16, nombre: "NVIDIA GeForce RTX 4070 Super", marca: "NVIDIA", modelo: "GeForce RTX 4070 Super", categoria: "video", stock: 5, precio: 45199, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 17, nombre: "AMD Radeon RX 7900 GRE", marca: "AMD", modelo: "Radeon RX 7900 GRE", categoria: "video", stock: 8, precio: 18999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 18, nombre: "NVIDIA GeForce RTX 4070", marca: "NVIDIA", modelo: "GeForce RTX 4070", categoria: "video", stock: 12, precio: 8999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 19, nombre: "NVIDIA GeForce RTX 4090", marca: "NVIDIA", modelo: "GeForce RTX 4090", categoria: "video", stock: 6, precio: 21999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 20, nombre: "AMD Radeon RX 7900 XTX", marca: "AMD", modelo: "Radeon RX 7900 XTX", categoria: "video", stock: 3, precio: 12999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 21, nombre: "Seagate Barracuda 4TB", marca: "Seagate", modelo: "Barracuda", categoria: "almacenamiento", stock: 5, precio: 8999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 22, nombre: "Western Digital Blue 2TB", marca: "Western Digital", modelo: "Blue", categoria: "almacenamiento", stock: 8, precio: 6999, imagen: "../assets/images/productos/motherboard_generic.jpg" },
-    { id: 23, nombre: "Toshiba P300 3TB", marca: "Toshiba", modelo: "P300", categoria: "almacenamiento", stock: 12, precio: 7999, imagen: "../assets/images/productos/motherboard_generic.jpg" }
-]
-
-
-//------------------------------------------------------------------
-
 // vacia el div de tarjetas
 function vaciarDiv() {
     contenedorTarjetas = document.getElementById("containerTarjetas")
@@ -43,6 +12,7 @@ function listarTodos(productos, carrito) {
     const productosOrdenados = productos.map(producto => producto)
     listarTarjeta(productosOrdenados, contenedorTarjetas, carrito)
     productosListados.push(...productosOrdenados)
+
 }
 
 // lista por categorías
@@ -50,7 +20,7 @@ function filtrarPorCategoria(productos, categoria, carrito) {
     vaciarDiv()
     productosListados = []
     const productosOrdenados = productos.filter(producto => producto.categoria === categoria)
-    listarTarjeta(productosOrdenados, contenedorTarjetas, carrito)
+    listarTarjeta(productosOrdenados, contenedorTarjetas, carrito, productos)
     productosListados.push(...productosOrdenados)
 }
 
@@ -65,7 +35,7 @@ function buscarProductos(productos, textoBusqueda, carrito) {
         const modeloCoincide = producto.modelo.toLowerCase().includes(textoBusqueda)
         return nombreCoincide || categoriaCoincide || marcaCoincide || modeloCoincide
     })
-    listarTarjeta(productosOrdenados, contenedorTarjetas, carrito)
+    listarTarjeta(productosOrdenados, contenedorTarjetas, carrito, productos)
     productosListados.push(...productosOrdenados)
 }
 
@@ -123,7 +93,7 @@ function listarTarjeta(productos, contenedor, carrito) {
                 <p class="tarjeta-producto-precio">$${producto.precio}</p>
                 <div class="linea-separadora"></div>
                 <div class="container-botones">
-                    <button class="btn-producto btn-ver-producto">Ver</button>
+                    
                     <button class="btn-producto btn-agregar-al-carrito" id=btnAgregarAlCarrito${producto.id}>Agregar al carrito</button>
                 </div>
                 `
@@ -165,6 +135,7 @@ function agregarProductoAlCarrito(e, carrito, productos) {
 
 // listar carrito
 function listarCarrito(carrito, contenedor) {
+
     carrito = cargarCarrito()
     contenedorTarjetas = document.getElementById("containerTarjetas")
     contenedorTarjetas.className = "tarjeta-carrito"
@@ -219,12 +190,7 @@ function listarCarrito(carrito, contenedor) {
                 text: "No hay ningún producto en el carrito",
             })
         } else {
-            carrito.forEach(productoCarrito => {
-                const productoEnStock = productos.find(producto => producto.id === productoCarrito.id)
-                if (productoEnStock) {
-                    productoEnStock.stock -= productoCarrito.cantidad
-                }
-            })
+
             localStorage.removeItem("carrito")
             carrito = []
             vaciarDiv()
@@ -245,8 +211,18 @@ function cargarCarrito() {
 
 
 // funcion principal
-function principal() {
+async function principal() {
 
+    fetch("../JSON/productos.json")
+    .then(response =>  response.json())
+    .then(productos => {
+        return productos
+    })
+
+
+    const response = await fetch("../JSON/productos.json")
+    const productos = await response.json()
+     
     let carrito = []
     let carritoLocalStorage = JSON.parse(localStorage.getItem("carrito"))
     if (carritoLocalStorage) {
@@ -268,6 +244,12 @@ function principal() {
     //boton memorias
     const btnCategoriasMemoria = document.getElementById("btnCategoriasMemoria")
     btnCategoriasMemoria.onclick = () => filtrarPorCategoria(productos, "memoria RAM", carrito)
+    //boton placas de video
+    const btnCategoriasVideo = document.getElementById("btnCategoriasVideo")
+    btnCategoriasVideo.onclick = () => filtrarPorCategoria(productos, "video", carrito)
+    //boton placas de video
+    const btnCategoriasAlmacenamiento = document.getElementById("btnCategoriasAlmacenamiento")
+    btnCategoriasAlmacenamiento.onclick = () => filtrarPorCategoria(productos, "almacenamiento", carrito)
     // selector opciones sort
     const opcion = document.getElementsByClassName("selectorSort")[0]
     opcion.onclick = () => opcionSort(opcion, carrito)
